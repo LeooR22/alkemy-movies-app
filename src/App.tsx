7532088eed03b82dd3803bc13d63994ec418fd2b
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-06-23 14:44:12 -0300

    Router & Provider connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Login } from "./components/Login";
+import { FC, ReactNode } from "react";
+import { AuthProvider } from "./context/auth";
+import AppRouter from "./routers/AppRouter";
+
+interface Props {
+  children: ReactNode;
+}
+
+const AppState: FC<Props> = ({ children }) => {
+  return <AuthProvider>{children}</AuthProvider>;
+};
 
 export const App = () => {
   return (
     <>
-      <Login />
+      <AppState>
+        <AppRouter />
+      </AppState>
     </>
   );
 };

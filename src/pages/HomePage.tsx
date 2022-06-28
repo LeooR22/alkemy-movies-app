68db92198778168e93f15a74af62d807f95d361f
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-06-27 22:21:11 -0300

    show data of useMovies

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
+import { useMovies } from "../hooks/useMovies";
 export const HomePage = () => {
+  const { isLoading, nowPlaying } = useMovies();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  console.log(nowPlaying);
+
   return <div>HomePage</div>;
 };
